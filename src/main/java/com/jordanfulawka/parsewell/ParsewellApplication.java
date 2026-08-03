867887d116d146311b5f1e9d9b9d896f9412f384
jordanfulawka <98784255+jordanfulawka@users.noreply.github.com>
2026-08-03 19:08:00 -0400

    first commit

---
+++ src/main/java/com/jordanfulawka/parsewell/ParsewellApplication.java
@@ -0,0 +1,13 @@
+package com.jordanfulawka.parsewell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParsewellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParsewellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jordanfulawka/parsewell/ParsewellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jordanfulawka.parsewell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParsewellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
